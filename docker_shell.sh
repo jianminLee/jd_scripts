5e471c191ec70ab5dceaba954cdd747a9b72b774
Lijianmin <lijianmin520@gmail.com>
2021-03-16 23:41:21 +0800

    update

--- docker_shell.sh
+++ docker_shell.sh
@@ -10,7 +10,6 @@
  fi
  ## 拷贝脚本到/scripts/目录下，免得安装依赖
  cp ./jd_dreamFactory.js /scripts/jd_dreamFactory_orz.js
- cp ./update_crontab.sh /scripts/docker/update_crontab.sh
 
 # #自定义增加crontab任务
  ## sleep $((RANDOM % $RANDOM_DELAY_MAX)); 延迟执行，确保docker配置了RANDOM_DELAY_MAX环境变量
@@ -18,5 +17,5 @@
  # echo "59,0,1,2,3,4,5 0,9,11,13,15,17,19,20,21,22,23 * * *  node /scripts/jd_live_redrain_offical_mod.js >> /scripts/logs/jd_live_redrain_offical_mod.log 2>&1" >> /scripts/docker/merged_list_file.sh
 
 if ! [ ! -f "/scripts/docker/export_jd_cookies_script_orz.sh" ]; then
-  echo "* * * * * sh +x /scripts/docker/update_crontab.sh 2>&1" >> /scripts/docker/merged_list_file.sh
+  echo "* * * * * sh +x /jd_scripts_orz/update_crontab.sh 2>&1" >> /scripts/docker/merged_list_file.sh
 fi
