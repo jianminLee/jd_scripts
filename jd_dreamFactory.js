089ab88a2b80ff13d75d3a821e08c3d9fc343d53
Lijianmin <lijianmin520@gmail.com>
2021-03-24 20:26:14 +0800

    fix getUrlQueryParams bug

--- jd_dreamFactory.js
+++ jd_dreamFactory.js
@@ -1687,14 +1687,14 @@ function getUrlQueryParams(url_string, param)
       let data = url.searchParams.get(param);
       return data ? data : '';
   } else {
-      if (url_string.indexOf("?") != -1) {    //判断是否有参数
-      var strSub =null;                
-      var str = url_string.split("?");//根据？截取url
-      var strs = str[1].split("&");//str[1]为获取？号后的字符串，并且根据&符号进行截取，得到新的字符串数组，这个字符串数组中有n个key=value字符串
-      for (var i = 0; i < strs.length; i++) {//遍历字符串数组
-        strSub = strs[i].split("=");//取第i个key=value字符串，并用“=”截取获得新的字符串数组 这个数组里面第0个字符是key，第1个字符value
-        if (strSub[0] == param) {//判断第0个字符与该方法的参数key是否相等，如果相等 则返回对应的值value。
-          return strSub[1];
+    if (url_string.indexOf("?") != -1) {    //判断是否有参数
+      let strSub =null;
+      let str = url_string.split("?");
+      let strs = str[1].split("&");
+      for (var i = 0; i < strs.length; i++) {
+        strSub = strs[i].split("=");
+        if (strSub[0] == param) {
+          return strs[i].substr(strs[i].indexOf('=')+1);
         }
       }
     }
